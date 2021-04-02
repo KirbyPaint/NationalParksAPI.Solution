a390d224f55ef59040c58f19fdb48fcf4be0cbe2
Ashley Porter <porter.ashley13@gmail.com>
2021-04-02 10:16:45 -0700

    add Startup.cs data and correct namespace in Program.cs, project now builds

--- NationalParksAPI/Program.cs
+++ NationalParksAPI/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace MessageBoard
+namespace NationalParksAPI
 {
   public class Program
   {
