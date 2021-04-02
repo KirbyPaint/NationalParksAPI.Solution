a7d0cf4df1adefe42fb8521eec4470fc72ee881b
Ashley Porter <porter.ashley13@gmail.com>
2021-04-02 09:23:42 -0700

    modify data structure for better location data input

--- NationalParksAPI/Models/Park.cs
+++ NationalParksAPI/Models/Park.cs
@@ -5,31 +5,29 @@ namespace NationalParksAPI.Models
 {
   public class Park
   {
+    [Required]
     public int ParkId { get; set; }
 
     [Required]
-    [StringLength(255)]
     public string Name { get; set; }
 
     [Required]
     public string Description { get; set; }
 
     [Required]
-    public string Street { get; set; }
+    public double Longitude { get; set; }
 
     [Required]
-    public string City { get; set; }
+    public double Latitude { get; set; }
 
     [Required]
     public int StateId { get; set; }
-    
-    [Required]
-    public int Zip { get; set; }
+
+    public string ImageUrl { get; set; }
+
 
     // Implement if time allows
     // public DateTime OpenDate { get; set; }
     // public DateTime ClosedDate { get; set; }
-
-    public string ImageUrl { get; set; }
   }
 }
