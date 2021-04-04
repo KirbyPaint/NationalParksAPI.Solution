d44a601d7de6f79aa10d197b56333a4105390fb2
Ashley Porter <porter.ashley13@gmail.com>
2021-04-03 23:49:35 -0700

    restore code to main branch

--- NationalParksAPI/Controllers/ParksController.cs
+++ NationalParksAPI/Controllers/ParksController.cs
@@ -118,5 +118,114 @@ namespace NationalParksAPI.Controllers
     {
       return _db.Parks.Any(e => e.ParkId == parkId);
     }
+
+    // Version 2.0
+    [HttpGet("all")]
+    [MapToApiVersion("2.0")]
+    public async Task<ActionResult<IEnumerable<Park>>> GetV2_0()
+    {
+      var query = _db.Parks.AsQueryable();
+      return await query.ToListAsync();
+    }
+
+    [HttpGet("{parkId}")]
+    [MapToApiVersion("2.0")]
+    public async Task<ActionResult<Park>> GetParkV2_0(int parkId)
+    {
+      var park = await _db.Parks.FindAsync(parkId);
+
+      if (park == null)
+      {
+        return NotFound();
+      }
+
+      return park;
+    }
+
+    [HttpGet]
+    [MapToApiVersion("2.0")]
+    public async Task<ActionResult<IEnumerable<Park>>> GetV2_0(string name, string description, double longitude, double latitude)
+    {
+      var query = _db.Parks.AsQueryable();
+
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name.Contains(name));
+      }
+      if (description != null)
+      {
+        query = query.Where(entry => entry.Description.Contains(description));
+      }
+      if (latitude >= -90 && latitude <= 90 && longitude >= -180 && longitude <= 180)
+      {
+        query = query.Where(entry => entry.Longitude <= (longitude + 0.5) && entry.Longitude >= (longitude - 0.5) && entry.Latitude <= (latitude + 0.5) && entry.Latitude >= (latitude - 0.5));
+      }
+
+      return await query.ToListAsync();
+    }
+
+    [HttpPost("{stateId}/add")]
+    [MapToApiVersion("2.0")]
+    public async Task<ActionResult<Park>> PostV2_0(Park park, int stateId)
+    {
+      var state = _db.States.Include(entry => entry.Parks).FirstOrDefault(entry => entry.StateId == stateId);
+      park.StateId = stateId;
+      state.Parks.Add(park);
+      _db.States.Update(state);
+      _db.Parks.Add(park);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+
+    [HttpPut("edit/{parkId}")]
+    [MapToApiVersion("2.0")]
+    public async Task<ActionResult> PutV2_0(int parkId, Park park)
+    {
+      if (parkId != park.ParkId)
+      {
+        return NotFound();
+      }
+
+      _db.Entry(park).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!ParkExistsV2_0(parkId))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    [HttpDelete("delete/{parkId}")]
+    [MapToApiVersion("2.0")]
+    public async Task<ActionResult> DeleteParkV2_0(int parkId)
+    {
+      var park = await _db.Parks.FindAsync(parkId);
+      if (park == null)
+      {
+        return NotFound();
+      }
+
+      _db.Parks.Remove(park);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+    private bool ParkExistsV2_0(int parkId)
+    {
+      return _db.Parks.Any(e => e.ParkId == parkId);
+    }
   }
 }
